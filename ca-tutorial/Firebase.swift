0dafafe873ad253e8b1e88c4b5a7ef21a5d3fd64
falcowinkler <falcowinkler@icloud.com>
2021-12-13 19:22:59 +0100

    build a compiler bomb that works with newer xcode versions

--- ca-tutorial/Firebase.swift
+++ ca-tutorial/Firebase.swift
@@ -8,22 +8,68 @@ struct FirebaseAnalytics {
     }
 }
 
-/* uncomment for a ~1min compiler bomb
- public let __tmp0 = 2 * 2 * 2 * 2.0 / 2 + 2
- public let __tmp1 = 2 * 2 * 2 * 2.0 / 2 + 2
- public let __tmp2 = 2 * 2 * 2 * 2.0 / 2 + 2
- public let __tmp3 = 2 * 2 * 2 * 2.0 / 2 + 2
- public let __tmp4 = 2 * 2 * 2 * 2.0 / 2 + 2
- public let __tmp5 = 2 * 2 * 2 * 2.0 / 2 + 2
- public let __tmp6 = 2 * 2 * 2 * 2.0 / 2 + 2
- public let __tmp7 = 2 * 2 * 2 * 2.0 / 2 + 2
- public let __tmp8 = 2 * 2 * 2 * 2.0 / 2 + 2
- public let __tmp9 = 2 * 2 * 2 * 2.0 / 2 + 2
- */
-
 protocol FirebaseAnalyticsProtocol {
     func track(eventName: String, data: [String: String])
 }
 
 // Wrap the 3rd party dependency in a protocol, so that we can test our analytics client
 extension FirebaseAnalytics: FirebaseAnalyticsProtocol {}
+
+// Compiler bomb:
+/*
+import Darwin
+public func a1(x:Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func a2(x:Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func a3(x:Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func a4(x:Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func a5(x:Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func a6(x:Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func a7(x:Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func a8(x:Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func a9(x: Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func b1(x:Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func b2(x:Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func b3(x:Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func b4(x:Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func b5(x:Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func b6(x:Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func b7(x:Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func b8(x:Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+public func b9(x: Double) -> Double {
+    return 1 - abs(x * 6 - floor(x * 3) * 2 - 1)
+}
+*/
